7ef9adf148656eb8c80979ab5516c8ecc1fcab2b
Antoine Lehurt <keuwah@gmail.com>
2015-04-02 10:12:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# detector-webgl
+
+Detect if WebGL is enabled.
+
+## Install
+
+With [npm](http://npmjs.org) do:
+
+```bash
+$ npm install detector-webgl --save
+```
+
+## Usage
+
+```js
+var isWebglEnabled = require('detector-webgl');
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = (function() {
+  var c = document.createElement('canvas');
+  try {
+    return !!window.WebGLRenderingContext
+      && (!!c.getContext('experimental-webgl') || !!c.getContext('webgl'));
+  } catch (e) {
+    return false;
+  }
+}());
